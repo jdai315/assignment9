0885b85a6e90bc618645c4a40e20ec882215fe73
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-12-05 13:18:33 -0500

    Backend: Thank you apge

--- survey-thanks.php
+++ survey-thanks.php
@@ -1,11 +1,4 @@
 <?php
-// Create a database connection
-require_once 'config.php';
-$sql = "SELECT * FROM `survey_assignment_9`";
-$result = mysqli_query($mysqli,$sql);
-?>
-
-<?php
     $name = $_GET['name'];
 ?>
 
