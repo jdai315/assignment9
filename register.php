9fc387bf5af3a1a2319ae4cc2a6c510cb7bfe60c
Jerry Dai <jdai315@gmail.com>
2017-12-05 02:33:38 -0500

    redirect successful register to admin page

--- register.php
+++ register.php
@@ -78,7 +78,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: login.php");
+                $_SESSION['username'] = $username;
+                header("location: admin.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
